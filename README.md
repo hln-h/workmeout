a02393db5146530f4bf75278c988fcdac0aee08f
helenhoang <helenhoang@helenhoangs-MacBook-Pro.local>
2021-11-09 12:30:45 +0100

    form inputs working

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # workmeout
+
+## Name
+
+Work Me Out
+
+## Description
+
+Creates a workout for you based on information such as time, parts of the body you wish to train, and equipment available.
+
+## Features
+
+## Visuals
+
+See README-support for screenshots.
+
+## Installation
+
+Usage
+Use examples liberally, and show the expected output if you can. It's helpful to have inline the smallest example of usage that you can demonstrate, while providing links to more sophisticated examples if they are too long to reasonably include in the README.
+
+## Support
+
+## Roadmap
+
+If you have ideas for releases in the future, it is a good idea to list them in the README.
+
+## Authors and acknowledgment
+
+Helen Hoang
+
+## Project status
+
+If you have run out of energy or time for your project, put a note at the top of the README saying that development has slowed down or stopped completely. Someone may choose to fork your project or volunteer to step in as a maintainer or owner, allowing your project to keep going. You can also make an explicit request for maintainers.
+
+_This is a student project that was created at [CodeOp](http://codeop.tech), a full stack development bootcamp in Barcelona._
--- helen-mvp/client/src/App.js
+++ helen-mvp/client/src/App.js
@@ -1,7 +1,56 @@
 import "./App.css";
+import React, { useState } from "react";
 
-function App() {
-  return <div className="App"></div>;
+export default function App() {
+  let [workout, setWorkout] = useState({
+    time: 0,
+    equipment: "",
+    bodyPart: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { value, name } = e.target;
+    setWorkout((workout) => ({ ...workout, [name]: value }));
+  };
+
+  const createWorkout = (e) => {
+    e.preventDefault();
+    console.log(workout);
+  };
+
+  const { time, equipment, bodyPart } = workout;
+  return (
+    <div className="App">
+      <h1>Work Me Out</h1>
+      <form onSubmit={createWorkout}>
+        Create a workout:
+        <input
+          placeholder="Minutes"
+          name="time"
+          value={time}
+          type="text"
+          onChange={handleInputChange}
+        />
+        <select name="equipment" value={equipment} onChange={handleInputChange}>
+          <option value="barbell">Barbell</option>
+          <option value="bench">Bench</option>
+          <option value="dumbell">Dumbell</option>
+          <option value="mat">Mat</option>
+          <option value="inclineBench">Incline Bench</option>
+          <option value="kettlebell">Kettlebell</option>
+          <option value="none">None- bodyweight</option>
+          <option value="pullupBar">Pull-up Bar</option>
+          <option value="swissBall">Swiss Ball</option>
+          <option value="szBar">SZ Bar</option>
+        </select>
+        <select name="bodyPart" value={bodyPart} onChange={handleInputChange}>
+          <option value="upperBody">Upper Body</option>
+          <option value="lowerBody">Lower Body</option>
+          <option value="fullBody">Full Body</option>
+          <option value="core">Abs</option>
+        </select>
+        <button type="submit">Work me out!</button>
+      </form>
+    </div>
+  );
 }
-
-export default App;
